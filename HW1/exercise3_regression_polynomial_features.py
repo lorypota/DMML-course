33e592a9a20119e7c5be1c0f66ce04a883df158a
salva24 <delatorregonzalezsalvador@gmail.com>
2024-11-22 16:14:58 +0100

    coments 3

--- HW1/exercise3_regression_polynomial_features.py
+++ HW1/exercise3_regression_polynomial_features.py
@@ -1,20 +1,24 @@
 import numpy as np
 import sklearn
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+#get the data
 print('The scikit-learn version is {}.'.format(sklearn.__version__))
 from sklearn.datasets import fetch_california_housing
 california = fetch_california_housing()
-print(california.DESCR)
+# print(california.DESCR)
 D = california.data
 y = california.target
 n,d = D.shape
-print(n,d)
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+
+#Scale de date
 scaler = StandardScaler()
-aff = PolynomialFeatures(2,include_bias=True)
 D_normalized = scaler.fit_transform(D)
+
+
+aff = PolynomialFeatures(2,include_bias=True)
 X = aff.fit_transform(D_normalized)
 aff.get_feature_names_out(california.feature_names)
-
+#2.a
 print(f"Number of samples: {n}")
 print(f"Number of features: {d}")
 print(f"Design matrix shape: {X.shape}")
