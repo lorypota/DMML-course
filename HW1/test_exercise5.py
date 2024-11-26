aced68a160f294dffd8abb1d2ff3a0156db0670d
Lorenzo Rota <lorenzokaide@gmail.com>
2024-11-26 18:29:45 +0100

    Minor changes

--- HW1/test_exercise5.py
+++ HW1/test_exercise5.py
@@ -17,11 +17,13 @@ D_test = vectorizer.transform(test.data)
 probabilities = proportional_probabilities(categories=categories, target=train.target)
 for i, category in enumerate(categories):
     print(f'Probability of {category}, y={i}: {probabilities[i]}')
+print('\n')
 
 #5b
 for i, category in enumerate(categories):
     print(f"log(p('naive' | y={i})) = {log_laplace_proabability_word_given_target(
         "naive", i, D_train, train, vectorizer, 10**-5)}")
+print('\n')
 
 #5c
 def compute_posterior(word, category):
@@ -34,7 +36,7 @@ def compute_posterior(word, category):
         categories=categories
     )
 
-print(f"p(y = 0|x_auto = 1) = {compute_posterior('auto', 0)}")
-print(f"p(y = 1|x_motorcycles = 1) = {compute_posterior('motorcycles', 1)}")
-print(f"p(y = 2|x_baseball = 1) = {compute_posterior('baseball', 2)}")
-print(f"p(y = 3|x_hockey = 1) = {compute_posterior('hockey', 3)}")
+print(f"p(y = 0 | x_auto = 1) = {compute_posterior('auto', 0)}")
+print(f"p(y = 1 | x_motorcycles = 1) = {compute_posterior('motorcycles', 1)}")
+print(f"p(y = 2 | x_baseball = 1) = {compute_posterior('baseball', 2)}")
+print(f"p(y = 3 | x_hockey = 1) = {compute_posterior('hockey', 3)}")
