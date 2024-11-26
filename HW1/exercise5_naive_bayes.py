03695ff859edb88f631dd8b6e4cb53e413215c65
Lorenzo Rota <lorenzokaide@gmail.com>
2024-11-26 12:32:31 +0100

    Minor changes

--- HW1/exercise5_naive_bayes.py
+++ HW1/exercise5_naive_bayes.py
@@ -4,28 +4,32 @@ import numpy as np
 def proportional_probability(target, categories):
     results = {}
     counts = np.bincount(target)
+    length = len(target)
     for i, category in enumerate(categories):
-        results[category] = counts[i] / len(target)
+        results[category] = counts[i] / length
     return results
 
 #5b
-def log_laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha):
-    return np.log(laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha))
-def laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha):
-    column_index=np.where(vectorizer.get_feature_names_out() == word)[0][0]
-    #we filter by category and we only focus on the given word column
-    dense_matrix = matrix.toarray()  
+def log_laplace_proabability_word_given_target(word, category, matrix, dataset, vectorizer, alpha):
+    return np.log(laplace_proabability_word_given_target(word, category, matrix, dataset, vectorizer, alpha))
+
+
+def laplace_proabability_word_given_target(word, category, matrix, dataset, vectorizer, alpha):
+    column_index = np.where(vectorizer.get_feature_names_out() == word)[0][0]
+    # filter by category and only focus on the given word column
+    dense_matrix = matrix.toarray()
     filtered_data = [row[column_index] for row, target in zip(dense_matrix, dataset.target) if target == category]
 
-    number_of_repetitions_category=len(filtered_data)
+    number_of_repetitions_category = len(filtered_data)
 
-    if(number_of_repetitions_category==0):
+    if (number_of_repetitions_category == 0):
         print("Error, target no present in data")
         return 0.
-    
-    word_repeticion=sum(filtered_data)
 
-    # number_possible_values= np.unique(dataset[:, column_index].toarray().flatten()).size#should be 2 since it is binary
+    word_repeticion = sum(filtered_data)
+
+    # number_possible_values= np.unique(dataset[:, column_index].toarray().flatten()).size
+    # 2 since it is binary
     return (word_repeticion+alpha)/(number_of_repetitions_category+alpha*2)
 
 #5c
--- HW1/test_exercise5.py
+++ HW1/test_exercise5.py
@@ -5,22 +5,21 @@ from exercise5_naive_bayes import *
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-categories = ['rec.autos', 'rec.motorcycles',
-              'rec.sport.baseball', 'rec.sport.hockey']
+categories = ['rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
 train = fetch_20newsgroups(subset='train', categories=categories)
 test = fetch_20newsgroups(subset='test', categories=categories)
-vectorizer = CountVectorizer(stop_words="english", min_df=5,token_pattern="[^\W\d_]+", binary=True)
+vectorizer = CountVectorizer(
+    stop_words="english", min_df=5, token_pattern="[^\W\d_]+", binary=True)
 D_train = vectorizer.fit_transform(train.data)
 D_test = vectorizer.transform(test.data)
 
 #5a
-probabilities = proportional_probability(categories=categories, target=train.target)
-for i,category in enumerate(categories):
+probabilities = proportional_probability(
+    categories=categories, target=train.target)
+for i, category in enumerate(categories):
     print(f'Probability of {category}, y={i}: {probabilities[category]}')
 
 #5b
-for i,category in enumerate(categories):
-    print(f'Probability of {category}, y={i}: {log_laplace_proabability_word_given_target("naive",i,D_train,train,vectorizer,10**-5)}')
-
-
-
+for i, category in enumerate(categories):
+    print(f"log(p('naive' | y={i})) = {log_laplace_proabability_word_given_target(
+        "naive", i, D_train, train, vectorizer, 10**-5)}")
