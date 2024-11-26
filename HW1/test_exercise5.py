421e1eabcbb2942c7efd0ac3dbe9d27bd6e2dca8
Lorenzo Rota <lorenzokaide@gmail.com>
2024-11-26 10:43:20 +0100

    5a

--- HW1/test_exercise5.py
+++ HW1/test_exercise5.py
@@ -1,8 +1,13 @@
 import numpy as np
 import sklearn
 from sklearn.datasets import fetch_20newsgroups
+from exercise5_naive_bayes import *
 
 categories = ['rec.autos', 'rec.motorcycles',
               'rec.sport.baseball', 'rec.sport.hockey']
 train = fetch_20newsgroups(subset='train', categories=categories)
 test = fetch_20newsgroups(subset='test', categories=categories)
+
+probabilities = proportional_probability(categories=categories, dataset=train.target)
+for category in categories:
+    print(f'Probability of {category}: {probabilities[category]}')
