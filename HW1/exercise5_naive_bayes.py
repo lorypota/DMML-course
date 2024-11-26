4b9d0b1ea20fcee2664ecbac0777578608492076
salva24 <delatorregonzalezsalvador@gmail.com>
2024-11-26 12:07:17 +0100

    5b

--- HW1/exercise5_naive_bayes.py
+++ HW1/exercise5_naive_bayes.py
@@ -9,3 +9,22 @@ def proportional_probability(target, categories):
     return results
 
 #5b
+def log_laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha):
+    return np.log(laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha))
+def laplace_proabability_word_given_target(word,category,matrix,dataset,vectorizer,alpha):
+    column_index=np.where(vectorizer.get_feature_names_out() == word)[0][0]
+    #we filter by category and we only focus on the given word column
+    dense_matrix = matrix.toarray()  
+    filtered_data = [row[column_index] for row, target in zip(dense_matrix, dataset.target) if target == category]
+
+    number_of_repetitions_category=len(filtered_data)
+
+    if(number_of_repetitions_category==0):
+        print("Error, target no present in data")
+        return 0.
+    
+    word_repeticion=sum(filtered_data)
+
+    # number_possible_values= np.unique(dataset[:, column_index].toarray().flatten()).size#should be 2 since it is binary
+    return (word_repeticion+alpha)/(number_of_repetitions_category+alpha*2)
+
--- HW1/test_exercise5.py
+++ HW1/test_exercise5.py
@@ -2,15 +2,25 @@ import numpy as np
 import sklearn
 from sklearn.datasets import fetch_20newsgroups
 from exercise5_naive_bayes import *
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 categories = ['rec.autos', 'rec.motorcycles',
               'rec.sport.baseball', 'rec.sport.hockey']
 train = fetch_20newsgroups(subset='train', categories=categories)
 test = fetch_20newsgroups(subset='test', categories=categories)
+vectorizer = CountVectorizer(stop_words="english", min_df=5,token_pattern="[^\W\d_]+", binary=True)
+D_train = vectorizer.fit_transform(train.data)
+D_test = vectorizer.transform(test.data)
 
 #5a
 probabilities = proportional_probability(categories=categories, target=train.target)
-for category in categories:
-    print(f'Probability of {category}: {probabilities[category]}')
+for i,category in enumerate(categories):
+    print(f'Probability of {category}, y={i}: {probabilities[category]}')
 
 #5b
+for i,category in enumerate(categories):
+    print(f'Probability of {category}, y={i}: {log_laplace_proabability_word_given_target("naive",i,D_train,train,vectorizer,10**-5)}')
+
+
+
