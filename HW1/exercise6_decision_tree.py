cfc8e511c23e8324aedd582df3a12eac2d69f72d
raijimlar <raijimlar@alum.us.es>
2024-11-26 12:15:24 +0100

    shorten

--- HW1/exercise6_decision_tree.py
+++ HW1/exercise6_decision_tree.py
@@ -8,8 +8,6 @@ def gini_impurity(targets):
 
 #6b
 def split_cost(L0_y, L1_y, L_y):
-    gini_L0 = gini_impurity(L0_y)
-    gini_L1 = gini_impurity(L1_y)
-    gini_L = gini_impurity(L_y)
+    gini_L0, gini_L1, gini_L = gini_impurity(L0_y), gini_impurity(L1_y), gini_impurity(L_y)
     N0, N1, N = len(L0_y), len(L1_y), len(L_y)
     return (N0 / N) * gini_L0 + (N1 / N) * gini_L1 - gini_L
