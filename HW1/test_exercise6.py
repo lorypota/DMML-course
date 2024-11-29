146770bca1451c712bcd09cb14267813d3ccdcc8
salva24 <delatorregonzalezsalvador@gmail.com>
2024-11-29 16:32:51 +0100

    5b

--- HW1/test_exercise6.py
+++ HW1/test_exercise6.py
@@ -11,6 +11,5 @@ print(f"Gini impurity at the root node: {root_gini}")
 #6b
 split_value = np.mean(D[:, 0])
 L0_y, L1_y = y[D[:, 0] <= split_value], y[D[:, 0] > split_value]
-L0_indices, L1_indices = D[:, 0] <= split_value, D[:, 0] > split_value
 cost = split_cost(L0_y, L1_y, y)
 print(f"Cost of the split: {cost}")
