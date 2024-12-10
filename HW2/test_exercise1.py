85949a81d8e9cbd03f1d9ac4a91d9b9f136b1cc1
Lorenzo Rota <lorenzokaide@gmail.com>
2024-12-10 12:00:08 +0100

    1c

--- HW2/test_exercise1.py
+++ HW2/test_exercise1.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
 from exercise1_PCA import PCA, plot_principal_components
 from sklearn.datasets import fetch_olivetti_faces
+import numpy as np
+
 faces = fetch_olivetti_faces()
 D = faces.data
 
@@ -13,3 +16,19 @@ low_dimensional_data, V_r = PCA(D, 4)
 eighth_data_point_coordinates = low_dimensional_data[7]
 print("Coordinates of the 8th data point in the low-dimensional space:",
       eighth_data_point_coordinates)
+
+# 1c
+low_dimensional_data, V_r = PCA(D, 90)
+low_dim_vector = np.array([0.45] * 45 + [-0.45] * 45)
+
+# Reconstruct the face
+reconstructed_face = np.dot(low_dim_vector, V_r) + np.mean(D, axis=0)
+
+# Reshape into 2D for visualization
+reconstructed_image = reconstructed_face.reshape((64, 64))
+
+# Plot the reconstructed face
+plt.imshow(reconstructed_image, cmap='gray')
+plt.title("Reconstructed Face")
+plt.axis('off')
+plt.show()
