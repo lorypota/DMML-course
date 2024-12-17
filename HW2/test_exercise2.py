97a016b76b6b3e7e23475247d358d2036d6f372c
salva24 <delatorregonzalezsalvador@gmail.com>
2024-12-17 11:28:02 +0100

    2c fixed a

--- HW2/test_exercise2.py
+++ HW2/test_exercise2.py
@@ -2,30 +2,30 @@ from exercise2_k_means import *
 from sklearn.neighbors import radius_neighbors_graph, kneighbors_graph
 import matplotlib.pyplot as plt
 
-# #2b
-# dataID, D, labels, r = generateBlobs(epsilon=0.05, n=500)
+#2b
+dataID, D, labels, r = generateBlobs(epsilon=0.05, n=500)
 
-# X_init = init_centroids_greedy_pp(D, r=3, l=10)
-# X, Y = kmeans(D, r, X_init)
+X_init = init_centroids_greedy_pp(D, r=3, l=10)
+X, Y = kmeans(D, r, X_init)
 
-# # Decode the cluster assignments from Y matrix
-# cluster_labels = np.argmax(Y, axis=1)
+# Decode the cluster assignments from Y matrix
+cluster_labels = np.argmax(Y, axis=1)
 
-# # Compute the mean approximation error
-# mean_error = mean_approximation_error(D, X, Y)
-# print(f"Mean approximation error: {mean_error}")
+# Compute the mean approximation error
+mean_error = mean_approximation_error(D, X, Y)
+print(f"Mean approximation error: {mean_error}")
 
-# # Evaluate clustering using Normalized Mutual Information (NMI)
-# nmi_score = normalized_mutual_info_score(labels, cluster_labels)
-# print(f"Normalized Mutual Information (NMI) score: {nmi_score}")
+# Evaluate clustering using Normalized Mutual Information (NMI)
+nmi_score = normalized_mutual_info_score(labels, cluster_labels)
+print(f"Normalized Mutual Information (NMI) score: {nmi_score}")
 
-# fig = plt.figure()
-# ax = plt.axes()
-# ax.axis('equal')
-# ax.scatter(D[:, 0], D[:, 1], c=np.argmax(Y,axis=1), s=10)
-# ax.scatter(X_init.T[:, 0], X_init.T[:, 1], c='red', s=50, marker = 'D') # initial centroids are in red
-# ax.scatter(X.T[:, 0], X.T[:, 1], c='blue', s=50, marker = 'D') # computed centroids are in blue
-# plt.show()
+fig = plt.figure()
+ax = plt.axes()
+ax.axis('equal')
+ax.scatter(D[:, 0], D[:, 1], c=np.argmax(Y,axis=1), s=10)
+ax.scatter(X_init.T[:, 0], X_init.T[:, 1], c='red', s=50, marker = 'D') # initial centroids are in red
+ax.scatter(X.T[:, 0], X.T[:, 1], c='blue', s=50, marker = 'D') # computed centroids are in blue
+plt.show()
 
 #2c
 dataID, D, labels, r = generateMoons(0.05,n=500)
