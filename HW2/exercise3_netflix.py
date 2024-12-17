350dfaf86d99b93e0d5ae2e1cb6df44a00738cc2
matteoggg <95472288+matteoggg@users.noreply.github.com>
2024-12-17 12:46:47 +0100

    work in progr

--- HW2/exercise3_netflix.py
+++ HW2/exercise3_netflix.py
@@ -1,9 +1,58 @@
 import numpy as np
 
 
-def matrix_completion(D, r, n, d, t_max=100, λ=0.1):
+def matrix_completion(D, n, d, r=20, t_max=100, λ=0.1):
     np.random.seed(0)
     X = np.random.normal(size=(d, r))
     Y = np.random.normal(size=(n, r))
-    # Implement now the optimization procedure
+
+    O = (D != 0).astype(int)
+    for t in range(1, t_max):
+        for k in range(1, d):
+            O_Xk = np.diag(O[:, k])  # Diagonal matrix for column k
+            # Update X_k using the formula
+            fact = Y.T @ O_Xk @ Y + λ * np.eye(r)
+            fact = np.linalg.inv(fact)
+            X[k, :] =  D[:, k].T @ Y @ fact
+             
+        for i in range(1, n):
+            O_Yi = np.diag(O[i, :])  # Diagonal matrix for row i
+            # Update Y_i using the formula
+            fact = X.T @ O_Yi @ X + λ * np.eye(r)
+            fact = np.linalg.inv(fact)
+            Y[i, :] =  D[i, :] @ X @ fact
+
     return X, Y
+
+def average_squared_error(D, X, Y):
+    """
+    Compute the average squared approximation error on the observed entries.
+
+    :param D: (np.ndarray) Original data matrix.
+    :param X: (np.ndarray) Learned factorized matrix X.
+    :param Y: (np.ndarray) Learned factorized matrix Y.
+    :return: (float) Average squared approximation error.
+    """
+
+    O = (D != 0).astype(int)
+
+    # Reconstructed matrix
+    D_hat = Y @ X.T
+
+    # Element-wise multiplication to keep only observed entries
+    error_matrix = O * (D - D_hat)
+
+    # Sum of squared errors
+    squared_error = np.sum(error_matrix ** 2)
+
+    # Total number of observed entries
+    num_observed = np.sum(O)
+
+    # Average squared error
+    avg_squared_error = squared_error / num_observed
+    return avg_squared_error
+
+
+def est_ratings(D, X, Y):
+    D_hat = Y @ X.T
+    print(D_hat)
--- HW2/test_exercise3.py
+++ HW2/test_exercise3.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+from exercise3_netflix import matrix_completion, average_squared_error, est_ratings
+
+MOVIES_PATH = "ml-latest-small/movies.csv"
+RATINGS_PATH = "ml-latest-small/ratings.csv"
 
 # Load the data
-movies = pd.read_csv('ml-latest-small/movies.csv')
-ratings = pd.read_csv('ml-latest-small/ratings.csv', sep=',')
+movies = pd.read_csv(MOVIES_PATH)
+ratings = pd.read_csv(RATINGS_PATH, sep=',')
 
 # Convert sparse data representation into a matrix
 # Fill unobserved entries with μ = 0
@@ -23,3 +27,11 @@ df_D = df_D.loc[keep_user, :]
 
 # Final preprocessed matrix
 D = df_D.to_numpy()
+n, d = D.shape
+print(n,d)
+
+
+X, Y = matrix_completion(D=D, n=n, d=d)
+avg_sq_err = average_squared_error(D=D, X=X, Y=Y)
+print(f"AVG squared error:{avg_sq_err}")
+est_ratings(D, X, Y)
