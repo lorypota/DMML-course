af60323805c5f56262f99574a0f76938b10df687
salva24 <delatorregonzalezsalvador@gmail.com>
2024-12-20 16:33:49 +0100

    4b

--- HW2/test_exercise4.py
+++ HW2/test_exercise4.py
@@ -1,18 +1,28 @@
 import torch
 import numpy as np
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import time
+import os
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch
 cuda = torch.cuda.is_available()
 device = 'cuda' if cuda else 'cpu'
-device
+print(device)
+if torch.cuda.is_available():
+    print(torch.cuda.get_device_name(0))
 # hyper parameters to play with
-dropout_1 = 0.9
-dropout_2 = 0.9
-lr = 0.9
+dropout_1 = 0.2
+dropout_2 = 0.2
+lr = 0.1
 momentum = 0.9
 weight_decay = 5e-4
 batch_size = 128
 epochs = 5
-import torch.nn as nn
-import torch.nn.functional as F
 
 # The embedding architecture returns the 
 # output of the penultimate layer
@@ -98,33 +108,6 @@ class Net(nn.Module):
         out = self.embed(x)
         return F.softmax(self.classifier(out),dim=1)
 
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
-#classes = ('0', '1', '2', '3', '4', '5')
-c=10
-
-# Data
-print('==> Preparing data..')
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-
-train_data = datasets.MNIST(root='./data', train=True, download=True, transform=trans)
-
-# Select only some classes 
-idx = train_data.targets < c
-train_data.targets = train_data.targets[idx]
-train_data.data = train_data.data[idx]
-trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
-
-
-testset = datasets.MNIST(root='./data', train=False, download=True, transform=trans)
-# Select only some classes 
-idx = testset.targets < c
-testset.targets = testset.targets[idx]
-testset.data = testset.data[idx]
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-
-import time
 def train_epoch(net, criterion, optimizer, trainloader, verbose=False):
     train_loss, correct, conf = 0, 0, 0
     start_time=time.time()
@@ -153,6 +136,8 @@ def train_epoch(net, criterion, optimizer, trainloader, verbose=False):
     n=len(trainloader.dataset)
     print('Loss: %.3f | Acc: %.3f%% (%d/%d) | Conf %.2f | time (s): %.2f'% (train_loss/len(trainloader), 100.*correct/n, correct, n, 100*conf/n, execution_time))
     return (100.*correct/n, 100*conf/n)
+
+
   
 def test_acc(net, criterion, data_loader):
     net.eval()
@@ -172,43 +157,7 @@ def test_acc(net, criterion, data_loader):
     print('Loss: %.3f | Acc: %.3f%% (%d/%d) | Conf %.2f'% (test_loss/max(len(data_loader),1), 100.*correct/n, correct, n, 100*conf/n))
     return (100.*correct/n, 100*conf/n)
 
-d=2
-# Model
-print('==> Building model..')
-classifier = nn.Linear(d, c, bias=True)
-net = Net(embedding_dim=d, classifier=classifier)
-net = net.to(device)
 
-criterion = nn.CrossEntropyLoss()
-
-sgd = torch.optim.SGD([{'params': net.parameters()},],
-                lr=lr, momentum=momentum, weight_decay=weight_decay)
-
-import os
-for epoch in range(epochs):
-    print('\nEpoch: %d' % epoch)
-    train_epoch(net, criterion, sgd, trainloader)
-    (acc,conf) = test_acc(net,criterion, testloader)
-
-print('Saving..')
-state = {'net': net.state_dict(),'acc': acc}
-if not os.path.isdir('checkpoint'):
-    os.mkdir('checkpoint')
-torch.save(state, './checkpoint/net.t7')
-
-import os
-for epoch in range(epochs):
-    print('\nEpoch: %d' % epoch)
-    train_epoch(net, criterion, sgd, trainloader)
-    (acc,conf) = test_acc(net,criterion, testloader)
-
-print('Saving..')
-state = {'net': net.state_dict(),'acc': acc}
-if not os.path.isdir('checkpoint'):
-    os.mkdir('checkpoint')
-torch.save(state, './checkpoint/net.t7')
-
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 def scatter_pictures(inputs, outputs, samples=30):
     zoom = 0.7
         
@@ -218,7 +167,7 @@ def scatter_pictures(inputs, outputs, samples=30):
         ab = AnnotationBbox(im, (outputs[j,0], outputs[j,1]), xycoords='data', frameon=False, alpha=0.5)
         ax.add_artist(ab)
 
-import matplotlib.pyplot as plt
+
 def plot_conf(conf, show_class_assignment=False, x_max=20, y_max=20, x_min=-1, y_min=-1):
     x = np.arange(x_min, x_max, 0.05)
     y = np.arange(y_min, y_max, 0.05)
@@ -236,28 +185,90 @@ def plot_conf(conf, show_class_assignment=False, x_max=20, y_max=20, x_min=-1, y
         cb.set_label('Confidence')
     plt.axis('scaled')
 
-inputs, targets = next(iter(testloader)) #load a batch
-outputs = net.embed(inputs).detach()
-fig, ax = plt.subplots(figsize=(8,5))
-plot_conf((lambda x: torch.softmax(net.classifier(x),dim=1)), x_max =max(outputs[:,0])+5, y_max =max(outputs[:,1])+5, x_min =min(outputs[:,0])-3, y_min =min(outputs[:,1])-3)
-scatter_pictures(inputs, outputs,samples=100)
-
-classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-c=10
-
-# Data
-print('==> Preparing data..')
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-
-train_data = datasets.FashionMNIST(root='./data', train=True, download=True, transform=trans)
-trainloader_fashion = DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
 
 
-testset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=trans)
-testloader_fashion = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=2)
+if __name__ == '__main__':
+    #classes = ('0', '1', '2', '3', '4', '5')
+    c=10
 
-inputs, targets = next(iter(testloader_fashion))
-outputs = net.embed(inputs).detach()
-fig, ax = plt.subplots(figsize=(8,5))
-plot_conf((lambda x: torch.softmax(net.classifier(x),dim=1)), x_max =max(outputs[:,0])+5, y_max =max(outputs[:,1])+5, x_min =min(outputs[:,0])-3, y_min =min(outputs[:,1])-3)
-scatter_pictures(inputs, outputs,samples=100)
+    # Data
+    print('==> Preparing data..')
+    trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+
+    train_data = datasets.MNIST(root='./data', train=True, download=True, transform=trans)
+
+    # Select only some classes 
+    idx = train_data.targets < c
+    train_data.targets = train_data.targets[idx]
+    train_data.data = train_data.data[idx]
+    trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
+
+
+    testset = datasets.MNIST(root='./data', train=False, download=True, transform=trans)
+    # Select only some classes 
+    idx = testset.targets < c
+    testset.targets = testset.targets[idx]
+    testset.data = testset.data[idx]
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    
+    d=2
+    # Model
+    print('==> Building model..')
+    classifier = nn.Linear(d, c, bias=True)
+    net = Net(embedding_dim=d, classifier=classifier)
+    net = net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    sgd = torch.optim.SGD([{'params': net.parameters()},],
+                    lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+    for epoch in range(epochs):
+        print('\nEpoch: %d' % epoch)
+        train_epoch(net, criterion, sgd, trainloader)
+        (acc,conf) = test_acc(net,criterion, testloader)
+
+    print('Saving..')
+    state = {'net': net.state_dict(),'acc': acc}
+    if not os.path.isdir('checkpoint'):
+        os.mkdir('checkpoint')
+    torch.save(state, './checkpoint/net.t7')
+
+    for epoch in range(epochs):
+        print('\nEpoch: %d' % epoch)
+        train_epoch(net, criterion, sgd, trainloader)
+        (acc,conf) = test_acc(net,criterion, testloader)
+
+    print('Saving..')
+    state = {'net': net.state_dict(),'acc': acc}
+    if not os.path.isdir('checkpoint'):
+        os.mkdir('checkpoint')
+    torch.save(state, './checkpoint/net.t7')
+
+    inputs, targets = next(iter(testloader)) #load a batch
+    outputs = net.embed(inputs).detach()
+    fig, ax = plt.subplots(figsize=(8,5))
+    plot_conf((lambda x: torch.softmax(net.classifier(x),dim=1)), x_max =max(outputs[:,0])+5, y_max =max(outputs[:,1])+5, x_min =min(outputs[:,0])-3, y_min =min(outputs[:,1])-3)
+    scatter_pictures(inputs, outputs,samples=100)
+
+    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    c=10
+
+    # Data
+    print('==> Preparing data..')
+    trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+
+    train_data = datasets.FashionMNIST(root='./data', train=True, download=True, transform=trans)
+    trainloader_fashion = DataLoader(train_data, batch_size=128, shuffle=True, num_workers=2)
+
+
+    testset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=trans)
+    testloader_fashion = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=2)
+
+    inputs, targets = next(iter(testloader_fashion))
+    outputs = net.embed(inputs).detach()
+    fig, ax = plt.subplots(figsize=(8,5))
+    plot_conf((lambda x: torch.softmax(net.classifier(x),dim=1)), x_max =max(outputs[:,0])+5, y_max =max(outputs[:,1])+5, x_min =min(outputs[:,0])-3, y_min =min(outputs[:,1])-3)
+    scatter_pictures(inputs, outputs,samples=100)
+
